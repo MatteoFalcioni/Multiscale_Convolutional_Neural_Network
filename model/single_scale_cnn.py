5b3d2637115cc22b834d806a6aca51bc52554284
MatteoFalcioni <matteo.falk@gmail.com>
2024-09-05 17:57:49 +0200

    Correcting GPT errors in 2nd and 3rd block

--- model/single_scale_cnn.py
+++ model/single_scale_cnn.py
@@ -42,19 +42,20 @@ class SingleScaleCNN(nn.Module):
         """
         super(SingleScaleCNN, self).__init__()
 
-        # First Convolutional Block
+        # First Convolutional Block: 3x128x128 --> 32x64x64  (recall: in Torch, channel dimension goes first)
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1)  # Output: 32x128x128
         self.bn1 = nn.BatchNorm2d(32)   # Batch normalization doesn't change the spatial dimension
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)  # Output: 32x64x64 (MaxPool changes spatial dimension)
 
-        # Second Convolutional Block
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, padding=1)
-        self.bn2 = nn.BatchNorm2d(32)
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        # Second Convolutional Block: 32x64x64 --> 64x32x32
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)  # Output: 64x64x64
+        self.bn2 = nn.BatchNorm2d(64)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)  # Output: 64x32x32
 
-        # Third Convolutional Block
-        self.conv3 = nn.Conv2d(in_channels=32, out_channels=128, kernel_size=3, padding=1)
+        # Third Convolutional Block: 64x32x32 --> 128x16x16
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)  # Output: 128x32x32
         self.bn3 = nn.BatchNorm2d(128)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)  # Output: 128x16x16
 
         # Fourth Convolutional Block
         self.conv4 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1)
