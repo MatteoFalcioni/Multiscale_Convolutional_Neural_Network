73dbd9626883c759a47003f751b899564aeeb410
MatteoFalcioni <matteo.falk@gmail.com>
2024-10-07 15:39:55 +0200

    updated main script to chanes

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ def main():
 
     train_loader, val_loader = prepare_dataloader(
         batch_size=args.batch_size,
-        data_dir=args.,
-        grid_save_dir='data/pre_processed_data_new',
+        data_dir=args.raw_data_filepath,
+        grid_save_dir='data/pre_processed_data/saved_grids',
         pre_process_data=True,
         window_sizes=[('small', 2.5), ('medium', 5.0), ('large', 10.0)],
         grid_resolution=128,
@@ -73,7 +73,7 @@ def main():
     # Run inference on a sample
     print("Starting inference process...")
 
-    data_array, _ = read_las_file_to_numpy(labeled_filepath)
+    data_array, _ = read_las_file_to_numpy(args.raw_data_filepath)
     # need to remap labels to match the ones in training. Maybe consider remapping already when doing las -> numpy ?
     remapped_array, _ = remap_labels(data_array)
     sample_array = remapped_array[np.random.choice(remapped_array.shape[0], 200, replace=False)]
